c825a3dcd539cf35c89ca83f9eddcbd7d6025bc7
feco9308 <77633055+feco9308@users.noreply.github.com>
2026-03-29 21:20:00 +0200

    Add files via upload

---
+++ firmware/teensy-ecu/legacy-rc/RCteensy.ino
@@ -0,0 +1,253 @@
+#include <Wire.h>
+#include <EEPROM.h> 
+#include <NativeEthernet.h>
+#include <NativeEthernetUdp.h>
+#include <Watchdog_t4.h>	// https://github.com/tonton81/WDT_T4
+#include "PCA95x5_RC.h"		// modified from https://github.com/hideakitai/PCA95x5
+
+#include <Adafruit_MCP23008.h>
+#include <Adafruit_MCP23X08.h>
+#include <Adafruit_MCP23X17.h>
+#include <Adafruit_MCP23XXX.h>
+
+#include <Adafruit_BusIO_Register.h>
+#include <Adafruit_I2CDevice.h>
+#include <Adafruit_I2CRegister.h>
+#include <Adafruit_SPIDevice.h>
+
+// rate control with Teensy 4.1
+# define InoDescription "RCteensy :  22-Feb-2024"
+const uint16_t InoID = 22024;	// change to send defaults to eeprom, ddmmy, no leading 0
+const uint8_t InoType = 1;		// 0 - Teensy AutoSteer, 1 - Teensy Rate, 2 - Nano Rate, 3 - Nano SwitchBox, 4 - ESP Rate
+
+#define MaxReadBuffer 100	// bytes
+#define MaxProductCount 2
+#define NC 0xFF		// Pins not connected
+#define ModStringLengths 15
+
+struct ModuleConfig
+{
+	uint8_t ID = 0;
+	uint8_t SensorCount = 2;        // up to 2 sensors, if 0 rate control will be disabled
+	uint8_t RelayOnSignal = 0;	    // value that turns on relays
+	uint8_t FlowOnDirection = 0;	// sets on value for flow valve or sets motor direction
+	uint8_t IP0 = 192;
+	uint8_t IP1 = 168;
+	uint8_t IP2 = 1;
+	uint8_t IP3 = 60;
+	uint8_t RelayControl = 1;		// 0 - no relays, 1 - GPIOs, 2 - PCA9555 8 relays, 3 - PCA9555 16 relays, 4 - MCP23017, 5 - PCA9685 single , 6 - PCA9685 paired
+	uint8_t ESPserialPort = 1;		// serial port to connect to wifi module
+	uint8_t RelayPins[16] = { 8,9,10,11,12,25,26,27,NC,NC,NC,NC,NC,NC,NC,NC };		// pin numbers when GPIOs are used for relay control (1), default RC11
+	uint8_t WifiMode = 1;			// 0 AP mode, 1 Station + AP
+	char NetName[ModStringLengths] = "Tractor";		// name of network ESP32 connects to
+	char NetPassword[ModStringLengths] = "111222333";
+};
+
+ModuleConfig MDL;
+
+struct SensorConfig
+{
+	uint8_t FlowPin;
+	uint8_t DirPin;
+	uint8_t PWMPin;
+	bool FlowEnabled;
+	double UPM;				// sent as upm X 1000
+	double PWM;
+	uint32_t CommTime;
+	byte ControlType;		// 0 standard, 1 combo close, 2 motor, 3 motor/weight, 4 fan, 5 timed combo
+	uint32_t TotalPulses;
+	double TargetUPM;
+	double MeterCal;
+	double ManualAdjust;
+	double KP;
+	double KI;
+	double KD;
+	byte MinPWM;
+	byte MaxPWM;
+	bool UseMultiPulses;	// 0 - time for one pulse, 1 - average time for multiple pulses
+	uint8_t Debounce;
+};
+
+SensorConfig Sensor[2];
+
+// ethernet
+EthernetUDP UDPcomm;
+uint16_t ListeningPort = 28888;
+uint16_t DestinationPort = 29999;
+IPAddress DestinationIP(MDL.IP0, MDL.IP1, MDL.IP2, 255);
+
+// AGIO
+EthernetUDP AGIOcomm;
+uint16_t ListeningPortAGIO = 8888;		// to listen on
+uint16_t DestinationPortAGIO = 9999;	// to send to
+
+// Relays
+byte RelayLo = 0;	// sections 0-7
+byte RelayHi = 0;	// sections 8-15
+byte PowerRelayLo;
+byte PowerRelayHi;
+byte InvertedLo;
+byte InvertedHi;
+
+const uint16_t LoopTime = 50;      //in msec = 20hz
+uint32_t LoopLast = LoopTime;
+const uint16_t SendTime = 200;
+uint32_t SendLast = SendTime;
+
+bool MasterOn = false;
+bool AutoOn = true;
+
+PCA9555 PCA;
+bool PCA9555PW_found = false;
+
+Adafruit_MCP23X17 MCP;
+bool MCP23017_found = false;
+
+int TimedCombo(byte, bool);	// function prototype
+
+uint32_t ESPtime;
+int8_t WifiStrength;
+
+// analog
+struct AnalogConfig
+{
+	int16_t AIN0;	// Pressure 0
+	int16_t AIN1;	// Pressure 1
+	int16_t AIN2;
+	int16_t AIN3;
+};
+AnalogConfig AINs;
+
+int ADS1115_Address;
+bool ADSfound = false;
+
+// WifiSwitches connection to ESP8266
+unsigned long WifiSwitchesTimer;
+bool WifiSwitchesEnabled = false;
+byte WifiSwitches[6];
+HardwareSerial* SerialESP;
+
+WDT_T4<WDT1> wdt;
+extern float tempmonGetTemp(void);
+bool GoodPins;	// configuration pins correct
+
+void setup()
+{
+	DoSetup();
+}
+
+void loop()
+{
+	SetPWM();
+
+	if (millis() - LoopLast >= LoopTime)
+	{
+		LoopLast = millis();
+
+		for (int i = 0; i < MDL.SensorCount; i++)
+		{
+			Sensor[i].FlowEnabled = (millis() - Sensor[i].CommTime < 5000)
+				&& ((Sensor[i].TargetUPM > 0 && MasterOn)
+					|| ((Sensor[i].ControlType == 4) && (Sensor[i].TargetUPM > 0))
+					|| (!AutoOn && MasterOn));
+		}
+
+		CheckRelays();
+		GetUPM();
+		AdjustFlow();
+		ReadAnalog();
+	}
+
+	if (millis() - SendLast > SendTime)
+	{
+		SendLast = millis();
+		SendData();
+	}
+
+	ReceiveSerial();
+	ReceiveUDPwired();
+	ReceiveAGIO();
+	ReceiveESP();
+	Blink();
+	wdt.feed();
+}
+
+byte ParseModID(byte ID)
+{
+	// top 4 bits
+	return ID >> 4;
+}
+
+byte ParseSenID(byte ID)
+{
+	// bottom 4 bits
+	return (ID & 0b00001111);
+}
+
+byte BuildModSenID(byte Mod_ID, byte Sen_ID)
+{
+	return ((Mod_ID << 4) | (Sen_ID & 0b00001111));
+}
+
+bool GoodCRC(byte Data[], byte Length)
+{
+	byte ck = CRC(Data, Length - 1, 0);
+	bool Result = (ck == Data[Length - 1]);
+	return Result;
+}
+
+byte CRC(byte Chk[], byte Length, byte Start)
+{
+	byte Result = 0;
+	int CK = 0;
+	for (int i = Start; i < Length; i++)
+	{
+		CK += Chk[i];
+	}
+	Result = (byte)CK;
+	return Result;
+}
+
+bool State = false;
+elapsedMillis BlinkTmr;
+elapsedMicros LoopTmr;
+byte ReadReset;
+uint32_t MaxLoopTime;
+
+//double debug1;
+//double debug2;
+
+void Blink()
+{
+	if (BlinkTmr > 1000)
+	{
+		BlinkTmr = 0;
+		State = !State;
+		digitalWrite(LED_BUILTIN, State);
+
+		//Serial.print(" Micros: ");
+		//Serial.print(MaxLoopTime);
+
+		//Serial.print(", IP Address: ");
+		//Serial.print(Ethernet.localIP());
+
+		//Serial.print(", Temp: ");
+		//Serial.print(tempmonGetTemp());
+
+		//Serial.print(", ");
+		//Serial.print(debug1);
+
+		//Serial.print(", ");
+		//Serial.print(debug2);
+
+		Serial.println("");
+
+		if (ReadReset++ > 10)
+		{
+			ReadReset = 0;
+			MaxLoopTime = 0;
+		}
+	}
+	if (LoopTmr > MaxLoopTime) MaxLoopTime = LoopTmr;
+	LoopTmr = 0;
+}
